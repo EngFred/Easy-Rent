6e231e221022485f3a04ffdadcf72c5f9ac8805d
EngFred <engfred88@gmail.com>
2024-07-03 17:15:01 +0300

    initial commit

--- app/src/main/java/com/kotlin/easyrent/core/theme/Theme.kt
+++ app/src/main/java/com/kotlin/easyrent/core/theme/Theme.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import com.kotlin.easyrent.core.presentation.SharedViewModel
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
--- app/src/main/java/com/kotlin/easyrent/features/auth/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/kotlin/easyrent/features/auth/ui/screens/login/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.kotlin.easyrent.features.auth.ui.screens.login
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
